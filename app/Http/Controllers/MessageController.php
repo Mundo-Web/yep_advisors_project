ff127ae80a882ed5807a8d46683137369eb8e749
Carlos Colina <carlosecolina89@gmail.com>
2024-05-03 16:50:38 -0500

    actualizacion

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -61,7 +61,169 @@ class MessageController extends Controller
         $mail = EmailConfig::config();
         try {
             $mail->addAddress($data['email']);
-            $mail->Body = "Buenas tardes $name su solicitud fue procesada";
+            $mail->Body = '
+                    <html lang="en">
+                    <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>Mundo web</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <style>
+                        * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        }
+                
+                        @font-face {
+                        font-family: grotesk;
+                        src: url(https://yepadvisors.com/fonts/PPRightGroteskCompactMedium.woff);
+                        font-weight: normal;
+                        }
+                    </style>
+                    </head>
+                    <body>
+                    <main>
+                        <table
+                        style="
+                            width: 600px;
+                            margin: 0 auto;
+                            text-align: center;
+                            background-image: url(https://yepadvisors.com/images/mail/fondo.png);
+                            background-repeat: no-repeat;
+                            background-position: center;
+                            background-size: cover;
+                        "
+                        >
+                        <thead>
+                            <tr>
+                            <th
+                                style="
+                                flex-direction: row;
+                                justify-content: center;
+                                align-items: center;
+                                margin: 100px;
+                                "
+                            >
+                                <img
+                                src="https://yepadvisors.com/images/mail/logo.png"
+                                alt="mundo web"
+                                />
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>
+                                <p
+                                style="
+                                    color: #050a41;
+                                    font-weight: 500;
+                                    font-size: 18px;
+                                    text-align: center;
+                                    width: 500px;
+                                    margin: 0 auto;
+                                    padding: 20px 0;
+                                    font-family: Montserrat, sans-serif;
+                                "
+                                >
+                                <span style="display: block">Hola </span>
+                                </p>
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>
+                                <p
+                                style="
+                                    color: #2d694b;
+                                    font-size: 40px;
+                                    line-height: 20px;
+                                    font-family: grotesk;
+                                "
+                                >
+                                <span style="display: block">' . $name . ' </span>
+                                </p>
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>
+                                <p
+                                style="
+                                    color: #2d694b;
+                                    font-size: 40px;
+                                    line-height: 70px;
+                                    font-family: grotesk;
+                                "
+                                >
+                                !Gracias
+                                <span style="color: #020303">por escribirnos! 💹</span>
+                                </p>
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>
+                                <p
+                                style="
+                                    color: #050a41;
+                                    font-weight: 500;
+                                    font-size: 18px;
+                                    text-align: center;
+                                    width: 500px;
+                                    margin: 0 auto;
+                                    padding: 20px 0;
+                                    font-family: Montserrat, sans-serif;
+                                "
+                                >
+                                En breve nos pondremos en contacto
+                                contigo.
+                                </p>
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>
+                                <a
+                                href="https://mundoweb.pe/"
+                                style="
+                                    text-decoration: none;
+                                    background-color: #2d694b;
+                                    color: white;
+                                    border-radius: 10px;
+                                    padding: 12px 16px;
+                                    display: inline-flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    gap: 10px;
+                                    font-weight: 600;
+                                    font-family: Montserrat, sans-serif;
+                                    font-size: 16px;
+                                "
+                                >
+                                <span>Visita nuestra web</span>
+                                
+                                </a>
+                            </td>
+                            </tr>
+                            <tr>
+                            <td style="text-align: right; padding-right: 30px">
+                                <img
+                                src="https://yepadvisors.com/images/mail/persona.png"
+                                alt="mundo web"
+                                style="width: 80%; margin-top: 100px"
+                                />
+                            </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </main>
+                    </body>
+                </html>
+          
+            ';
             $mail->isHTML(true);
             $mail->send();
             
