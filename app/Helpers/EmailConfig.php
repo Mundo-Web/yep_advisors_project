cae7f9314bb520845f2a8d2790fe7b2a8f39478f
Carlos Colina <carlosecolina89@gmail.com>
2024-05-06 14:58:44 -0500

    correcciones con cliente

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -13,13 +13,13 @@ class EmailConfig
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'hola@mundoweb.pe';
-        $mail->Password = 'rzhfwaxrddnppppr';
+        $mail->Username = 'info@yepadvisors.com';
+        $mail->Password = 'tszkfklxtoatwjbi';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
         $mail->Subject = 'Notificación de informacion Recibida';
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom('hola@mundoweb.pe', 'Mundo Web');
+        $mail->setFrom('info@yepadvisors.com', 'Yep Advisors');
         return $mail;
     }
 }
